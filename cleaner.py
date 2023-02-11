ded29c0dee9185dfd459ffbebed56873c728a3ed
Sultan Bazher <107263975+SultanCYB@users.noreply.github.com>
2023-02-11 10:46:08 +0300

    Create cleaner.py

---
+++ cleaner.py
@@ -0,0 +1,10 @@
+def cleaner(clean: list):
+    ''' for removing duplicated elemnts plus the original duplicated element'''
+
+    for i in clean:
+        if clean.count(i) != 1:
+
+            for s in range(clean.count(i)):
+                clean.remove(i)
+
+    return clean
